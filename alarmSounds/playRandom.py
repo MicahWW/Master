8e8c546a66da08207676c66e38f012723f13bd83
MicahWW <mwhornstab@gmail.com>
2018-08-17 00:13:24 -0500

    alarmsounds

---
+++ alarmSounds/playRandom.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+import random
+import dircache
+import os
+
+## it starts out in AlarmSounds and at random picks a folder, that folder is usually an artist name
+## it then picks another random folder from within the artist folder, that folder is usually an album name
+## lastly it randomly picks a file a random inside the album folder
+dir = 'alarmSounds/'
+artist = random.choice(dircache.listdir(dir))
+# updates dir path to the artist
+dir = os.path.join(dir, artist)
+album = random.choice(dircache.listdir(dir))
+# updates dir path to the album
+dir = os.path.join(dir, album)
+# picks a song
+song = random.choice(dircache.listdir(dir))
+song = os.path.join(dir, song)
+
+
+subprocess.call(["play", song])
