5e1017ff460b4f3fe0a89785071f162cdee43846
MicahWW <mwhornstab@gmail.com>
2018-09-17 22:20:52 -0500

    finished hangman New feature if you guess a letter that occurs more than once you get more lives

--- games/hangman.cpp
+++ games/hangman.cpp
@@ -2,13 +2,15 @@
 #include <string>
 #include <algorithm>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
+
 /* draw functions
 #################################################################################################*/
 
 // outputs the words with the characters that user has guessed
 // those that have not been guess are shown as "_"
-bool drawWord(string word, char used[]) {
+int drawWord(string word, char used[], int missed) {
 	// keeps track where it is at in the word
 	int wordPosition = 0;
 	// word length
@@ -47,13 +49,17 @@ bool drawWord(string word, char used[]) {
 		cout << used[usedPosition] << " ";
 		usedPosition += 1;
 	}
-	cout << endl;
+
+	int lives = 8 - missed;
+	cout << endl << "Correct: " << correct << "\tLives: " << lives << endl;
 
 	// checks if the word has been compleated
 	if(correct == (wordLength)) {
-		return true;
+		return 1;
+	} else if(lives == 0) {
+		return 2;
 	}
-	return false;
+	return 0;
 }
 
 
@@ -71,17 +77,23 @@ int drawPerson(string word, char used[]) {
                 usedPosition += 1;
         }
 
+
+	// will count the number of correct to find the number of missed
+	usedPosition = 0;
 	if(lettersUsed != 0) {
 		while(wordPosition < wordLength) {
 			if(word[wordPosition] == used[usedPosition]) {
 				correct += 1;
 				wordPosition += 1;
-				usedPosition += 1;
+				usedPosition = 0;
+			// not the end of the list
 			} else if(used[usedPosition+1] != '0') {
 				usedPosition += 1;
+			// reached the end of the list without finding the letter in list used
+			// advaces to the next letter in the word
 			} else {
 				wordPosition += 1;
-				usedPosition += 1;
+				usedPosition = 0;
 			}
 		}
 	}
@@ -119,7 +131,7 @@ int drawPerson(string word, char used[]) {
 	if(missed == 7) {
 		cout << "|     /" << endl;
 		remainingLines = 0;
-	}
+ 	}
 	if(missed >= 8) {
 		cout << "|     / \\" << endl;
 		remainingLines = 0;
@@ -132,16 +144,17 @@ int drawPerson(string word, char used[]) {
 
 	cout << "|___________";
 
-	return 0;
+	return missed;
 }
 
 // runs both draw funcions for ease of use in main()
-bool draw(string word, char used[]) {
-	drawPerson(word, used);
-	if(drawWord(word, used)) {
-		return true;
-	}
-	return false;
+int draw(string word, char used[]) {
+	int missed = drawPerson(word, used);
+	// tells if the word has been finished correctly or if no lives are left
+	// 	0 for not done
+	//	1 for word found correctly
+	// 	2 for no more lives
+	return drawWord(word, used, missed);
 }
 
 /*#########################################################################################
@@ -159,7 +172,7 @@ char userInput() {
 
 int main() {
 	// words that will be used for the game
-	string words[] = {"computer", "physics"};
+	string words[] = {"computer", "physics", "school", "study", "english", "something", "anything", "blah", "deskop", "laptop", "youtube", "google"};
 
 	// stores the letters that were used
 	// start with filling everthing with 0
@@ -173,22 +186,28 @@ int main() {
 	// reset usedPosition for futher use
  	usedPosition = 0;
 
+	srand(time(0));
 	// picks random word from words[]
 	// size of words[]
 	int size = sizeof(words) / sizeof(words[0]);
 	string word = words[rand() % size];
 
-	bool cont = true;
+	int cont = 0;
 
+	cout << "Guess the word!\n\tYou have eight lives,\n\tguessing a letter that is used more than once gives you more lives" << endl;
 	draw(word, used);
-	while(cont) {
+	while(!cont) {
                 used[usedPosition] = userInput();
                 usedPosition += 1;
+		cont = draw(word, used);
+	}
 
-		if(draw(word, used)) {
-			cout << "Congralations" << endl;
-			cont = false;
-		}
+	if(cont == 1) {
+		cout << "Congralations" << endl << endl;
+	} else if(cont == 2) {
+		cout << "You lose" << endl << endl;
+	} else {
+		cout << "ERROR ERROR" << endl;
 	}
 	return 0;
 }
