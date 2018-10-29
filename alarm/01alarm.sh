91e0f2ad6883e5367ee86e4c056c7775e48ff812
MicahWW <mwhornstab@gmail.com>
2018-10-29 11:42:35 -0500

    test bash copy converts tabs to ^I

--- alarm/01alarm.sh
+++ alarm/01alarm.sh
@@ -2,4 +2,7 @@ echo " "
 echo "Time must be in Military time"
 read -p "What time do you want to wake up? " wakeUp
 
-echo "python alarmSounds/playRandom.py" | at $wakeUp
+min=$(expr $wakeUp % 100)
+hour=$(expr $(expr $wakeUp - $min) / 100)
+
+echo $hour":"$min
